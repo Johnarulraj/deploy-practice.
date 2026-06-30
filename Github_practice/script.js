8ec3d1b8002bca3602cd3612dd135b510f553a9a
Johnarulraj <johnarulraj0396@gmail.com>
2026-06-30 14:39:10 +0530

    Delete Github_practice directory

--- Github_practice/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("countBtn");
-let count = 0;
-
-btn.addEventListener("click", () => {
-  count++;
-  btn.textContent = `Clicked ${count} time${count === 1 ? "" : "s"}`;
-});
